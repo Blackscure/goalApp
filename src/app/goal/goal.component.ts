d1cbc590457fcd5ed5020e4c28a4b440b07ceb9d
Blackscure <Wekesabuyahi@gmail.com>
2020-04-07 23:36:08 +0300

    --- Auto-Git Commit ---

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
 import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
 
 @Component({
   selector: 'app-goal',
@@ -13,11 +14,10 @@ import { GoalService } from '../goal-service/goal.service';
 export class GoalComponent implements OnInit {
 
   goal:Goal[];
+  alertService:AlertService;
   
 
-  constructor(goalService:GoalService) {
-    this.goal = goalService.getGoal()
-  }
+  
   ngOnInit(): void {
     throw new Error("Method not implemented.");
   }
@@ -32,15 +32,23 @@ export class GoalComponent implements OnInit {
 
       if (toDelete){
         this.goal.splice(index,1)
+        this.alertService.alertMe("The goal has been deleted")
       }
     }
   }
 
-   
+  constructor(goalService:GoalService, alertService:AlertService) {
+    this.goal = goalService.getGoal()
+    this.alertService = alertService;
+  }
+
+
   addNewGoal(goal){
     let goalLength = this.goal.length;
     goal.id = goalLength+1;
     goal.completeDate = new Date(goal.completeDate)
     this.goal.push(goal)
   }
+
+ 
 }
